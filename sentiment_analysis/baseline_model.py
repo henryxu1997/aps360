d1c4add375733b56b71549ed78fbb5bd521329b0
Jordan Lee <jordandlee@hotmail.ca>
2019-11-10 13:02:53 -0500

    Created baseline model for sentiment analysis. Updated some data processing functions

---
+++ sentiment_analysis/baseline_model.py
@@ -0,0 +1,38 @@
+import torch
+import torchtext
+
+from data_processing import split_text
+
+class BaselineModel:
+    """
+    Baseline model stores a list of positive and negative words.
+    When analyzing a new text, it counts the number of positive and negative words
+    in that text and uses that to determine the overall sentiment of the sentence.
+    """
+    def __init__(self):
+        with open('data/positive_words.txt') as f:
+            self.positive_words = set([s.strip() for s in f.readlines()])
+        with open('data/negative_words.txt') as f:
+            self.negative_words = set([s.strip() for s in f.readlines()])
+    
+    def get_sentiment(self, text):
+        words = split_text(text)
+        pos_count = 0
+        for word in words:
+            if word in self.positive_words:
+                # print('pos', word)
+                pos_count += 1
+            elif word in self.negative_words:
+                # print('neg', word)
+                pos_count -= 1
+        # If there's a balance of positive and negative words, return 0.5
+        if pos_count == 0:
+            return 0.5
+        # Else return positive or negative depending on which has more.
+        return 1.0 if pos_count > 0 else 0.0
+
+if __name__ == '__main__':
+    model = BaselineModel()
+    x = model.get_sentiment('This cake is good.')
+    y = model.get_sentiment('The big fox is nasty')
+    print(x, y)
--- sentiment_analysis/data_processing.py
+++ sentiment_analysis/data_processing.py
@@ -4,16 +4,22 @@ import torchtext
 EMBEDDING_SIZE = 50
 glove = torchtext.vocab.GloVe(name="6B", dim=EMBEDDING_SIZE)
 
+def split_text(text):
+    # Utility method to separate punctuation, split and convert words to lower case.
+    text = text.replace(".", " . ") \
+                .replace(",", " , ") \
+                .replace(";", " ; ") \
+                .replace("?", " ? ") \
+                .replace("!", " ! ")
+    return text.lower().split()
 
 def convert_words_to_embeddings(words):
     """
     words = List[str]
     """
-    print(type(glove[words[0]]))
     x = torch.zeros(size=[len(words), EMBEDDING_SIZE])
     for i, word in enumerate(words):
         x[i] = glove[word]
-    print(x)
     return x
 
 def load_sst_dataset():
@@ -23,5 +29,6 @@ def load_sst_dataset():
 
 
 if __name__ == '__main__':
-    test = 'the quick brown fox jumped over the green turtle'.split()
-    convert_words_to_embeddings(test)
+    test = split_text('the quick brown fox jumped over the green turtle. it was really exciting! HYPE?')
+    embedding = convert_words_to_embeddings(test)
+    print(embedding.shape, len(test))
