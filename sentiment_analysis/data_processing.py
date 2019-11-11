8fd92a3afe7596cacef54ee27f031426941be834
Ivan <29393345+hivanco@users.noreply.github.com>
2019-11-10 19:10:19 -0500

    Fixed SST labels to be float from 0 to 1

--- sentiment_analysis/data_processing.py
+++ sentiment_analysis/data_processing.py
@@ -1,3 +1,5 @@
+import os
+
 import torch
 import torchtext
 
@@ -28,22 +30,52 @@ def load_sst_dataset(batch_size=32, device='cpu', root='.data'):
     Set device='cuda' to use GPU.
     """
 
+    def get_label_value(label):
+        return {'0': 0.0, '1': 0.25, '2': 0.5, '3': 0.75, '4': 1.0,
+            None: None}[label]
+
     text_field = torchtext.data.Field(
         sequential=True, batch_first=True, include_lengths=True)
     label_field = torchtext.data.Field(
-        sequential=False, batch_first=True)
+        sequential=False, batch_first=True, use_vocab=False,
+        dtype=torch.float32,
+        preprocessing=torchtext.data.Pipeline(get_label_value))
 
-    train, val, test = torchtext.datasets.SST.splits(
-        text_field, label_field, root=root, fine_grained=True)
+    fields = [('text', text_field), ('label', label_field)]
 
-    text_field.build_vocab(train, vectors=glove)
-    label_field.build_vocab(train)
+    path = torchtext.datasets.SST.download(root)
+
+    train_file = 'train.txt'
+    valid_file = 'dev.txt'
+    test_file = 'test.txt'
+
+    train_path = os.path.join(path, train_file)
+    valid_path = os.path.join(path, valid_file)
+    test_path = os.path.join(path, test_file)
+
+    with open(os.path.expanduser(train_path)) as f:
+        train_examples = [torchtext.data.Example.fromtree(line, fields) for line in f]
+    with open(os.path.expanduser(valid_path)) as f:
+        valid_examples = [torchtext.data.Example.fromtree(line, fields) for line in f]
+    with open(os.path.expanduser(test_path)) as f:
+        test_examples = [torchtext.data.Example.fromtree(line, fields) for line in f]
+
+    train_data = torchtext.data.Dataset(train_examples, fields)
+    valid_data = torchtext.data.Dataset(valid_examples, fields)
+    test_data = torchtext.data.Dataset(test_examples, fields)
+
+    text_field.build_vocab(train_data, vectors=glove)
 
     return torchtext.data.BucketIterator.splits(
-        (train, val, test), batch_size=batch_size, device=device)
+        (train_data, valid_data, test_data), batch_size=batch_size, device=device)
 
 
 if __name__ == '__main__':
     test = split_text('the quick brown fox jumped over the green turtle. it was really exciting! HYPE?')
     embedding = convert_words_to_embeddings(test)
     print(embedding.shape, len(test))
+
+    # train_iter, valid_iter, test_iter = load_sst_dataset()
+    # for batch in train_iter:
+    #     print(batch.text)
+    #     print(batch.label)
